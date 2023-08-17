89280d1eebf72e3ac7b76faa3c4bb6428b4acd1a
blfuentes <epiphonetodie@gmail.com>
2023-08-17 13:05:32 +0200

    refactoring 2019 until day 13 part 1

--- AdventOfCode_2019/day05/part02/day05_part02.fs
+++ AdventOfCode_2019/day05/part02/day05_part02.fs
@@ -9,4 +9,4 @@ let filepath = __SOURCE_DIRECTORY__ + @"../../day05_input.txt"
 
 let execute(input:bigint) =
     let values = IntCodeModule.getInput filepath
-    IntCodeModule.getOutput values 0I 0I input input 2I true false 0I
+    (IntCodeModule.getOutput values 0I 0I input input 2I true false false 0I).Output
--- AdventOfCode_2019/day09/part01/day09_part01.fs
+++ AdventOfCode_2019/day09/part01/day09_part01.fs
@@ -5,4 +5,4 @@ open AoC_2019.Modules
 let filepath = __SOURCE_DIRECTORY__ + @"../../day09_input.txt"
 
 let execute =
-    IntCodeModule.getOutput (IntCodeModule.getInput filepath)  0I 0I 0I 1I 2I true false 0I
+    (IntCodeModule.getOutput (IntCodeModule.getInput filepath)  0I 0I 0I 1I 2I true false false 0I).Output
