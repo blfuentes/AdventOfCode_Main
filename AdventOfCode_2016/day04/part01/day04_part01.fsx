94dd2b8cdc56abd8393f3be2f11b4970f9b6b319
blfuentes <blfuentes@mundo-r.com>
2023-10-15 20:58:59 +0200

    Working on 20160401

--- AdventOfCode_2016/day04/part01/day04_part01.fsx
+++ AdventOfCode_2016/day04/part01/day04_part01.fsx
@@ -2,27 +2,28 @@
 
 open System
 open System.Collections.Generic
+open System.Text.RegularExpressions
 
 open AdventOfCode_2016.Modules
 
 //let path = "day04/test_input_01.txt"
 let path = "day04/day04_input.txt"
 
-let getValueOfRoom (room: string) =
-    let firstPart = room.ToCharArray() |> Array.takeWhile(fun c -> c <> '[')
-    let secondPart = room.ToCharArray() |> Array.skipWhile(fun c -> c <> '[') |> Array.filter(fun c -> c <> ']')
-    let letters = firstPart |> Array.takeWhile(fun c -> not (['0'..'9'] 
-                            |> List.contains c)) |> Array.filter(fun c -> c <> '-')
-    let number = int(System.String.Concat(firstPart |> Array.skipWhile(fun c -> not (['0'..'9'] |> List.contains c))))
-    let grouped = letters |> Array.countBy(fun c -> c) |> Array.sortByDescending(fun c -> snd c) |> Array.take 5
-    
-    let diffsCheck = grouped |> Array.groupBy(fun c -> snd c)
-    // let sameCheck = grouped |> Array.groupBy(fun c -> snd c) |> Array.filter(fun c -> (fst c > 1))
-    
-    let secondPartCheckSum = System.String.Concat(secondPart)
-    // let diffsOk = diffsCheck |> Array.map(fun c -> (fst (snd c)))
-    (diffsCheck, 0)
+let getParts(input: string) =
+    let input = "aaaaa-bbb-z-y-x-123[abxyz]"
+    let pattern = @"^(.*?)-(\d+)\[(.*?)\]$"
+    let regex = Regex(pattern)
+
+    match regex.Match(input) with
+    | matchResult when matchResult.Success ->
+        let groups = matchResult.Groups
+        let stringValue = groups.[1].Value
+        let numberValue = groups.[2].Value
+        let bracketsContent = groups.[3].Value
+        printfn "String: %s, Number: %s, Content in brackets: %s" stringValue numberValue bracketsContent
+    | _ ->
+        printfn "No match found in the input string."
 
 
 let inputLines = Utilities.GetLinesFromFile(path)
-getValueOfRoom "aaaaa-bbb-z-y-x-123[abxyz]"
+getParts "aaaaa-bbb-z-y-x-123[abxyz]"
