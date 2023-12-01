67711944af7a744c0ded574f5acf01c0fe1a196d
blfuentes <epiphonetodie@gmail.com>
2023-12-01 16:00:19 +0100

    improved 202301

--- AdventOfCode_2023/day01/part02/day01_part02.fs
+++ AdventOfCode_2023/day01/part02/day01_part02.fs
@@ -2,37 +2,29 @@
 
 open System
 open System.Collections.Generic
+open System.Text.RegularExpressions
 
 open AdventOfCode_2023.Modules
 
 let path = "day01/day01_input.txt"
 
-let findFirstIndexOfDigit (input: string) (digit: string * string) =
-    let indexDigit = input.IndexOf(fst digit)
-    let indexWord = input.IndexOf(snd digit)
-    match (indexDigit, indexWord) with
-    | (-1, _) -> indexWord
-    | (_, -1) -> indexDigit
-    | (d, w) -> if d < w then  d else w
+let convertDigit (input: string) =
+    match input with
+    | "one" -> 1
+    | "two" -> 2
+    | "three" -> 3
+    | "four" -> 4
+    | "five" -> 5
+    | "six" -> 6
+    | "seven" -> 7
+    | "eight" -> 8
+    | "nine" -> 9
+    | _ -> (int)input
 
-let findLastIndexOfDigit (input: string) (digit: string * string) =
-    let indexDigit = input.LastIndexOf(fst digit)
-    let indexWord = input.LastIndexOf(snd digit)
-    match (indexDigit, indexWord) with
-    | (-1, _) -> indexWord
-    | (_, -1) -> indexDigit
-    | (d, w) -> if d > w then  d else w
-
-let getFirstAndLast (input: string) (digits: (string * string) list) =
-    digits |> List.map (fun d -> [|(int)(fst d); findFirstIndexOfDigit input d; findLastIndexOfDigit input d|])
-
-let buildNumber (input: int array list) =
-    let first = (input |> List.filter(fun i -> i.[1] > -1) |> List.sortBy (fun i -> i.[1])).Head.[0]
-    let last = (input |> List.filter(fun i -> i.[2] > -1) |> List.sortByDescending (fun i -> i.[2])).Head[0]
-    first * 10 + last
-
-let validDigits = [("1", "one"); ("2", "two"); ("3", "three"); ("4", "four"); ("5", "five"); ("6", "six"); ("7", "seven"); ("8", "eight"); ("9", "nine")]
-
+let findNumbers input =
+    let firstNumber = Regex.Match(input, @"\d|one|two|three|four|five|six|seven|eight|nine").Value
+    let secondNumber = Regex.Match(input, @"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft).Value
+    convertDigit(firstNumber) * 10 + convertDigit(secondNumber)
 let execute =
     let lines = Utilities.GetLinesFromFile path
-    lines |> Array.map (fun l -> getFirstAndLast l validDigits) |> Array.sumBy buildNumber
+    lines |> Array.sumBy findNumbers
