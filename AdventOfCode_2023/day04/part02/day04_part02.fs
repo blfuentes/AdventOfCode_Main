6a301639ed8d0584243c7569a55c38ecc383941c
blfuentes <epiphonetodie@gmail.com>
2023-12-04 17:42:09 +0100

    refactored 202304

--- AdventOfCode_2023/day04/part02/day04_part02.fs
+++ AdventOfCode_2023/day04/part02/day04_part02.fs
@@ -6,51 +6,50 @@ open System.Text.RegularExpressions
 
 open AdventOfCode_2023.Modules
 
-//let path = "day04/test_input_01.txt"
 let path = "day04/day04_input.txt"
 
 type CardSet = {
     CardId: int
-    WinningCards: int array
-    MyCards: int array
-    IsOriginal: bool
+    WinningCards: int Set
+    MyCards: int Set
 }
 
 let buildCardSet (input: string) =
     let parts = input.Split(':')
     let cardId = Regex.Match(parts.[0], @"\d+").Value |> int
-    let winningCards = Regex.Matches(parts.[1].Split('|').[0], @"\d+") |> Seq.map _.Value |> Seq.map int |> Seq.distinct |> Seq.toArray
-    let myCards = Regex.Matches(parts.[1].Split('|').[1], @"\d+") |> Seq.map _.Value |> Seq.map int |> Seq.distinct |> Seq.toArray
-    { CardId = cardId; MyCards = myCards; WinningCards = winningCards; IsOriginal = true}
+    let winningCards = Regex.Matches(parts.[1].Split('|').[0], @"\d+") |> Seq.map _.Value |> Seq.map int |> Set.ofSeq
+    let myCards = Regex.Matches(parts.[1].Split('|').[1], @"\d+") |> Seq.map _.Value |> Seq.map int |> Set.ofSeq
+    { CardId = cardId; MyCards = myCards; WinningCards = winningCards }
 
-let countMatchingCards (cardSet: CardSet) =
-    cardSet.MyCards |> Seq.filter (fun x -> cardSet.WinningCards |> Seq.contains x) |> Seq.length
+let getMatchingCards (cardSet: CardSet) =
+    Set.intersect cardSet.WinningCards cardSet.MyCards
 
 let returnMatchingCards (cardSet: CardSet) (originalCardSet: CardSet list) =
-    let matchingCards = countMatchingCards cardSet
+    let matchingCards = (getMatchingCards cardSet).Count
     let copyCardsIds = if matchingCards > 0 then [cardSet.CardId + 1 .. cardSet.CardId + matchingCards] else []
     let copyCards = originalCardSet |> Seq.filter (fun x -> copyCardsIds |> Seq.contains x.CardId) |> Seq.toList
     copyCards
 
-let buildMemo (originalCardSet: CardSet list) =
-    originalCardSet |> List.map (fun c -> (c.CardId, returnMatchingCards c originalCardSet)) |> Array.ofList
-
 let cardSet (input: string array) =
     input |> Seq.map buildCardSet |> Seq.toList
 
-let rec playCardSet (initialCards: int array) (remainingCards: CardSet list) (memo: (int * CardSet list) array)=
+let rec playCardSet (initialCards: int array) (remainingCards: CardSet list)=
     match remainingCards with
-    | [] -> initialCards
+    | [] -> initialCards |> Array.sum
     | card :: tail ->
-        let copyCards = memo.[card.CardId - 1] |> snd
+        let copyCards = returnMatchingCards card remainingCards
         let numOfCurrentCards = initialCards.[card.CardId - 1]
-        for copy in copyCards do
-            initialCards.[copy.CardId - 1] <- initialCards.[copy.CardId - 1] + numOfCurrentCards
-        playCardSet initialCards tail memo
+        let newCards =
+            seq {
+                for idx in 0..initialCards.Length - 1 do
+                    if (copyCards |> List.exists (fun e -> e.CardId = idx + 1)) then
+                        yield (initialCards.[idx] + numOfCurrentCards)
+                    else
+                        yield initialCards.[idx]                   
+            } |> Array.ofSeq
+        playCardSet newCards tail
 
 let execute =
     let lines = Utilities.GetLinesFromFile path
-    let memo = buildMemo (cardSet lines)
     let initialCards = Array.create lines.Length 1
-    let ended = playCardSet initialCards (cardSet lines) memo
-    ended |> Array.sum
+    playCardSet initialCards (cardSet lines)
