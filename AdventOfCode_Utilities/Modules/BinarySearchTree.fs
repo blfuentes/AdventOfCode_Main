ee6f1ce2c453e63c75dd9ca6b2bf10870f649138
blfuentes <blfuentes@mundo-r.com>
2023-12-11 21:39:24 +0100

    added some helpers
    - binary search tree
    - graph

---
+++ AdventOfCode_Utilities/Modules/BinarySearchTree.fs
@@ -0,0 +1,96 @@
+﻿namespace AdventOfCode_Utilities.Modules.BinarySearthTree
+
+module BinarySearthTree = 
+    type Tree<'a> = 
+      | Empty
+      | Node of value: 'a * left: Tree<'a> * right: Tree<'a>
+
+    let isBST (tree : Tree<'a>) =
+      let rec verify lo hi tree =
+        match tree with
+        | Empty -> true
+        | Node (value, left, right) ->
+          match lo, hi with
+          | Some lo, _ when value < lo -> false
+          | _, Some hi when value > hi -> false
+          | _ ->
+            let hi' = defaultArg hi value |> min value |> Some
+            let lo' = defaultArg lo value |> max value |> Some
+            verify lo hi' left && verify lo' hi right
+    
+      verify None None tree
+
+    let rec insert newValue (tree : Tree<'a>) =
+      match tree with
+      | Empty -> Node (newValue, Empty, Empty)
+      | Node (value, left, right) when newValue < value ->
+        let left' = insert newValue left
+        Node (value, left', right)
+      | Node (value, left, right) when newValue > value ->
+        let right' = insert newValue right
+        Node (value, left, right')
+      | _ -> tree
+
+    let rec findInOrderPredecessor (tree : Tree<'a>) =
+      match tree with
+      | Empty -> Empty
+      | Node (_, _, Empty) -> tree
+      | Node (_, _, right) -> findInOrderPredecessor right 
+    
+    let rec delete value (tree : Tree<'a>) =
+      match tree with
+      | Empty -> Empty
+      | Node (value', left, right) when value < value' ->
+        let left' = delete value left
+        Node (value', left', right)
+      | Node (value', left, right) when value > value' ->
+        let right' = delete value right
+        Node (value', left, right')
+      | Node (_, Empty, Empty) ->
+        Empty
+      | Node (_, left, Empty) -> 
+        left
+      | Node (_, Empty, right) ->
+        right
+      | Node (_, left, right) ->
+        let (Node(value', _, _)) = findInOrderPredecessor left
+        let left' = delete value' left
+        Node (value', left', right)
+
+    // Process the tree in order: 1 2 3 4 5 6 - Breadth-First Traversal
+    let BFtraverse (tree : Tree<'a>) =
+      let rec loop (trees : seq<Tree<'a>>) = seq {
+        let values = 
+          trees
+          |> Seq.choose (function
+            | Empty -> None
+            | Node (value, _, _) -> Some value)
+    
+        yield! values
+    
+        let subtrees = 
+          trees 
+          |> Seq.collect (function 
+            | Empty -> Seq.empty
+            | Node (value, left, right) -> seq { yield left; yield right })
+          |> Seq.toArray
+    
+        if subtrees.Length > 0 then
+          yield! loop subtrees
+      }
+    
+      loop <| seq { yield tree }
+
+    // Process the tree in order: 1 2 4 5 3 6 - Depth-First Traversal
+    let DFtraverse (tree : Tree<'a>) =
+      let rec loop (tree : Tree<'a>) = seq {
+        match tree with
+        | Empty -> ()
+        | Node (value, left, right) ->
+          yield value
+    
+          yield! loop left
+          yield! loop right
+      }
+      
+      loop tree
---
+++ AdventOfCode_Utilities/Modules/Graph.fs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode_Utilities.Modules.Graph
+
+module Graph =
+    type Graph<'T when 'T: comparison> =
+        {
+            Nodes: Set<'T>
+            Edges: Set<('T * 'T)>
+        }
+        static member Empty = { Nodes = Set.empty; Edges = Set.empty }
+    
+    let addNode node (graph: Graph<'T>) =
+        { graph with Nodes = graph.Nodes.Add(node) }
+    
+    let addEdge edge (graph: Graph<'T>) =
+        { graph with Edges = graph.Edges.Add(edge) }
+    
+    let nodeExists node (graph: Graph<'T>) =
+        graph.Nodes.Contains(node)
+    
+    let getNeighbors node (graph: Graph<'T>) =
+        graph.Edges
+        |> Set.filter (fun (u, v) -> u = node || v = node)
+        |> Set.map (fun (u, v) -> if u = node then v else u)
+        |> Set.toList
+
+    let bfs start (graph: Graph<'T>) =
+        let rec bfsImpl (visited: Set<'T>) (queue: List<'T>) =
+            match queue with
+            | [] -> visited
+            | node :: tail ->
+                if not (visited.Contains(node)) then
+                    let neighbors = getNeighbors node graph
+                    bfsImpl (visited.Add(node)) (tail @ neighbors)
+                else
+                    bfsImpl visited tail
+        bfsImpl Set.empty [start]
+
+    let dfs start (graph: Graph<'T>) =
+        let rec dfsImpl (visited: Set<'T>) (stack: List<'T>) =
+            match stack with
+            | [] -> visited
+            | node :: tail ->
+                if not (visited.Contains(node)) then
+                    let neighbors = getNeighbors node graph
+                    dfsImpl (visited.Add(node)) (neighbors @ tail)
+                else
+                    dfsImpl visited tail
+        dfsImpl Set.empty [start]
+
+
+//let graph = 
+//    { Nodes = set ['A'; 'B'; 'C'; 'D'; 'E']
+//      Edges = set [('A', 'B'); ('A', 'C'); ('B', 'D'); ('C', 'E'); ('D', 'E')] }
+
+//let result = bfs 'A' graph
+//printfn "%A" result // Output: Set ['A'; 'B'; 'C'; 'D'; 'E']
