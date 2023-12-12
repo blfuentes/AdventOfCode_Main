2627fa2b29aa6fa98ab121eb1664084d9c54bbf0
blfuentes <epiphonetodie@gmail.com>
2023-12-12 17:57:03 +0100

    working on 202312

--- AdventOfCode_2023/day12/part01/day12_part01.fs
+++ AdventOfCode_2023/day12/part01/day12_part01.fs
@@ -1,5 +1,8 @@
 ﻿module day12_part01
 
+open System
+open System.Collections.Generic
+
 open AdventOfCode_2023.Modules
 open AdventOfCode_Utilities
 
@@ -23,17 +26,22 @@ let rec matchesDefinition (mask: char array) (testcase: char array) =
         else
             false
 
+let generatePart (index: int) (comb: int list) (conRecord: ConditionRecord) =
+    let generalPart = String.replicate conRecord.definition.[index] "#"
+    let splitPart = String.replicate comb.[index + 1] "."
+    generalPart + splitPart
+
 let rec generateCombinations (conRecord: ConditionRecord) (possibleSplits: int list list) (maxLength: int) (combinations: char array list)=
     match possibleSplits with
     | comb :: tail ->
         let start = String.replicate comb.[0] "."
-        let firstPart = String.replicate conRecord.definition.[0] "#"
-        let firstSplit = String.replicate comb.[1] "."
-        let secondPart = String.replicate conRecord.definition.[1] "#"
-        let secondSplit = String.replicate comb.[2] "."
-        let thirdPart = String.replicate conRecord.definition.[2] "#"
-        let ending = String.replicate comb.[3] "."
-        let newDefinition = (start + firstPart + firstSplit + secondPart + secondSplit + thirdPart + ending).ToCharArray()
+
+        let parts =
+            seq {
+                for idx in 0..conRecord.definition.Length - 1 do
+                    yield generatePart idx comb conRecord
+            } |> String.concat ""
+        let newDefinition = (start + parts).ToCharArray()
         let combinations' =
             if matchesDefinition conRecord.mask newDefinition then
                 combinations @ [newDefinition]
@@ -48,9 +56,11 @@ let getAllCombinations (lists: int list list) =
 
 let countPossible (conRecord: ConditionRecord) (separators: int list list) =
     let maxLength = conRecord.mask.Length
-    let valuesLength = conRecord.definition |> Array.sum
-    let possibleSplitCombinations = separators |> List.filter(fun l -> l.[0] + l.[1] + l.[2] + l.[3] + valuesLength = maxLength)
-    let validCombinations = generateCombinations conRecord possibleSplitCombinations maxLength []
+    //let valuesLength = conRecord.definition |> Array.sum
+    //let possibleSplitCombinations = separators |> List.filter(fun l -> (l |> List.sum) + valuesLength = maxLength)
+    let validCombinations = generateCombinations conRecord separators maxLength []
+    printfn "Combinations for %A" conRecord
+    //validCombinations |> List.iter(fun c -> printfn "%s" (String.Join("", c)))
     validCombinations.Length
 
 //let sample = "???.### 1,1,3"
@@ -61,11 +71,39 @@ let countPossible (conRecord: ConditionRecord) (separators: int list list) =
 //let separators = getAllCombinations [[0..10];[1..10]; [1..10]; [0..10]]
 //let result = countPossible conRecord separators
 
+//let getAuxLists (numberOfLists: int) =
+//    List.replicate (numberOfLists - 1) [1..13]
+
+let getAuxLists (numberOfLists: int) (maskl: int) (maxelem: int)=
+    List.replicate (numberOfLists - 1) [1..(maskl - numberOfLists - maxelem + 1)]
+
 let execute =
     //let path = "day12/test_input_01.txt"
-    let path = "day12/test_input_02.txt"
-    //let path = "day12/day12_input.txt"    
-    let lines = LocalHelper.ReadLines path |> Seq.map parseRecordEntry
-    let separators = getAllCombinations [[0..10];[1..10]; [1..10]; [0..10]]
-    let result = lines |> Seq.map(fun c -> countPossible c separators) |> Seq.sum
+    //let path = "day12/test_input_02.txt"
+    let path = "day12/day12_input.txt"    
+    let condRecords = LocalHelper.ReadLines path |> Seq.map parseRecordEntry |> List.ofSeq
+    let diffCondRecords = condRecords |> List.groupBy _.definition.Length 
+    let perms = new Dictionary<(int*int*int), int list list>()
+    for defLength in diffCondRecords do
+        for cr in (snd defLength) do
+            let numberOfLists = (fst defLength)
+            let maskl = cr.mask.Length
+            let maxelem = cr.definition |> Array.max
+            if not(perms.ContainsKey((numberOfLists, maskl, maxelem))) then
+                printfn "Generating perms for mask of %i and %i elements" (fst defLength) maxelem
+                let auxLists = getAuxLists numberOfLists maskl maxelem
+                let allLists = [[0..(maskl - numberOfLists - maxelem + 1)]] @ auxLists @ [[0..(maskl - numberOfLists - maxelem + 1)]]
+                let allperms = getAllCombinations allLists
+                perms.Add((numberOfLists, maskl, maxelem), allperms)
+
+    printfn "Starting the computation"
+    let result = 
+        condRecords |> Seq.map(fun conRecord -> 
+            let maxLength = conRecord.mask.Length
+            let valuesLength = conRecord.definition |> Array.sum
+            let maxElem = conRecord.definition |> Array.max
+            let firstParsing = perms[(conRecord.definition.Length, conRecord.mask.Length, maxElem)]
+            let secondParsing = firstParsing |> List.filter (fun p -> (p |> List.sum) + valuesLength = maxLength)
+            countPossible conRecord secondParsing
+        ) |> Seq.sum
     result
--- AdventOfCode_2023/day12/part01/day12_part01.fsx
+++ AdventOfCode_2023/day12/part01/day12_part01.fsx
@@ -26,17 +26,22 @@ let rec matchesDefinition (mask: char array) (testcase: char array) =
         else
             false
 
+let generatePart (index: int) (comb: int list) (conRecord: ConditionRecord) =
+    let generalPart = String.replicate conRecord.definition.[index] "#"
+    let splitPart = String.replicate comb.[index + 1] "."
+    generalPart + splitPart
+
 let rec generateCombinations (conRecord: ConditionRecord) (possibleSplits: int list list) (maxLength: int) (combinations: char array list)=
     match possibleSplits with
     | comb :: tail ->
         let start = String.replicate comb.[0] "."
-        let firstPart = String.replicate conRecord.definition.[0] "#"
-        let firstSplit = String.replicate comb.[1] "."
-        let secondPart = String.replicate conRecord.definition.[1] "#"
-        let secondSplit = String.replicate comb.[2] "."
-        let thirdPart = String.replicate conRecord.definition.[2] "#"
-        let ending = String.replicate comb.[3] "."
-        let newDefinition = (start + firstPart + firstSplit + secondPart + secondSplit + thirdPart + ending).ToCharArray()
+
+        let parts =
+            seq {
+                for idx in 0..conRecord.definition.Length - 1 do
+                    yield generatePart idx comb conRecord
+            } |> String.concat ""
+        let newDefinition = (start + parts).ToCharArray()
         let combinations' =
             if matchesDefinition conRecord.mask newDefinition then
                 combinations @ [newDefinition]
@@ -51,9 +56,11 @@ let getAllCombinations (lists: int list list) =
 
 let countPossible (conRecord: ConditionRecord) (separators: int list list) =
     let maxLength = conRecord.mask.Length
-    let valuesLength = conRecord.definition |> Array.sum
-    let possibleSplitCombinations = separators |> List.filter(fun l -> l.[0] + l.[1] + l.[2] + l.[3] + valuesLength = maxLength)
-    let validCombinations = generateCombinations conRecord possibleSplitCombinations maxLength []
+    //let valuesLength = conRecord.definition |> Array.sum
+    //let possibleSplitCombinations = separators |> List.filter(fun l -> (l |> List.sum) + valuesLength = maxLength)
+    let validCombinations = generateCombinations conRecord separators maxLength []
+    printfn "Combinations for %A" conRecord
+    //validCombinations |> List.iter(fun c -> printfn "%s" (String.Join("", c)))
     validCombinations.Length
 
 //let sample = "???.### 1,1,3"
@@ -64,13 +71,37 @@ let countPossible (conRecord: ConditionRecord) (separators: int list list) =
 //let separators = getAllCombinations [[0..10];[1..10]; [1..10]; [0..10]]
 //let result = countPossible conRecord separators
 
-let execute =
-    //let path = "day12/test_input_01.txt"
-    //let path = "day12/test_input_02.txt"
-    let path = "day12/day12_input.txt"    
-    let lines = LocalHelper.ReadLines path |> Seq.map parseRecordEntry
-    let separators = getAllCombinations [[0..10];[1..10]; [1..10]; [0..10]]
-    let result = lines |> Seq.map(fun c -> countPossible c separators) |> Seq.sum
-    result
+//let getAuxLists (numberOfLists: int) =
+//    List.replicate (numberOfLists - 1) [1..13]
 
+let getAuxLists (numberOfLists: int) (maskl: int)=
+    List.replicate (numberOfLists - 1) [1..(maskl - numberOfLists)]
+
+let execute =
+    let path = "day12/test_input_01.txt"
+    //let path = "day12/test_input_02.txt"
+    //let path = "day12/day12_input.txt"    
+    let condRecords = LocalHelper.ReadLines path |> Seq.map parseRecordEntry |> List.ofSeq
+    let diffCondRecords = condRecords |> List.groupBy _.definition.Length 
+    let perms = new Dictionary<(int*int), int list list>()
+    for defLength in diffCondRecords do
+        for cr in (snd defLength) do
+            let numOfLists = (fst defLength)
+            let maskl = cr.mask.Length
+            printfn "Generating perms for mask of %i and %i elements" (fst defLength) maskl
+            let auxLists = getAuxLists numOfLists maskl
+            let allLists = [[0..(maskl - numOfLists)]] @ auxLists @ [[0..(maskl - numOfLists)]]
+            let allperms = getAllCombinations allLists
+            perms.Add((fst defLength, cr.mask.Length), allperms)
+
+    printfn "Starting the computation"
+    let result = 
+        condRecords |> Seq.map(fun conRecord -> 
+            let maxLength = conRecord.mask.Length
+            let valuesLength = conRecord.definition |> Array.sum
+            let firstParsing = perms[(conRecord.definition.Length, conRecord.mask.Length)]
+            let secondParsing = firstParsing |> List.filter (fun p -> (p |> List.sum) + valuesLength = maxLength)
+            countPossible conRecord secondParsing
+        ) |> Seq.sum
+    result
 execute
