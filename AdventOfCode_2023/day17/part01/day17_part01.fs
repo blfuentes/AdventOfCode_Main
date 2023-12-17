46479d6a75dde9f34abf037da3f4343fd076bc7d
blfuentes <blfuentes@mundo-r.com>
2023-12-17 20:45:27 +0100

    refactoring 202317

--- AdventOfCode_2023/day17/part01/day17_part01.fs
+++ AdventOfCode_2023/day17/part01/day17_part01.fs
@@ -26,7 +26,7 @@ let doStep
     (grid: int[,]) (visited: Dictionary<(Direction*int), int>[,])
     (queue: PriorityQueue<(int*int*Direction*int), int>) (row: int) (col: int) 
     (dir: Direction) (currentHeat: int) 
-    (numOfMoves: int) (minSteps: int) (maxSteps: int) =
+    (numOfMoves: int) (maxSteps: int) =
     let dirRow =
         match dir with
         | UP -> -1
@@ -49,28 +49,25 @@ let doStep
             if not (outOfBounds newRow newCol) && newNumOfMoves <= maxSteps then
                 let newHeat = heat + grid[newRow, newCol]
                 let stop =
-                    if cc >= minSteps then
-                        let doContinue =
-                            if visited.[newRow, newCol].ContainsKey(dir, newNumOfMoves) then
-                                let tempHeat = visited.[newRow, newCol][(dir, newNumOfMoves)]
-                                tempHeat > newHeat
-                            else
-                                true
-                        if doContinue  then
-                            queue.Enqueue((newRow, newCol, dir, newNumOfMoves), newHeat)
-                            let newVisited = new Dictionary<(Direction*int), int>()
-                            for x in visited.[newRow, newCol] do
-                                newVisited.Add(x.Key, x.Value)
-                            newVisited[(dir, newNumOfMoves)] <- newHeat
-                            visited.[newRow, newCol] <- newVisited
-                        not doContinue
-                    else
-                        false
+                    let doContinue =
+                        if visited.[newRow, newCol].ContainsKey(dir, newNumOfMoves) then
+                            let tempHeat = visited.[newRow, newCol][(dir, newNumOfMoves)]
+                            tempHeat > newHeat
+                        else
+                            true
+                    if doContinue  then
+                        queue.Enqueue((newRow, newCol, dir, newNumOfMoves), newHeat)
+                        let newVisited = new Dictionary<(Direction*int), int>()
+                        for x in visited.[newRow, newCol] do
+                            newVisited.Add(x.Key, x.Value)
+                        newVisited[(dir, newNumOfMoves)] <- newHeat
+                        visited.[newRow, newCol] <- newVisited
+                    not doContinue
                 explore stop (cc + 1) newHeat
     
     explore false 1 currentHeat 
 
-let travel (grid: int[,]) (minSteps: int) (maxSteps: int) =
+let travel (grid: int[,]) (maxSteps: int) =
     let queue = new PriorityQueue<(int*int*Direction*int), int>()
     let visited = Array2D.create (grid.GetUpperBound(0) + 1) (grid.GetUpperBound(1) + 1) (new Dictionary<(Direction*int), int>())
     for i in 0..(grid.GetUpperBound(0) - 1) do
@@ -88,10 +85,10 @@ let travel (grid: int[,]) (minSteps: int) (maxSteps: int) =
                 0
 
         if (numOfMoves < maxSteps) then
-            doStep grid visited queue row col direction heat numOfMoves minSteps maxSteps
-        if (numOfMoves >= minSteps) then
-            doStep grid visited queue row col (turnLeft direction) heat 0 minSteps maxSteps
-            doStep grid visited queue row col (turnRight direction) heat 0 minSteps maxSteps
+            doStep grid visited queue row col direction heat numOfMoves maxSteps
+        if (numOfMoves > 0) then
+            doStep grid visited queue row col (turnLeft direction) heat 0 maxSteps
+            doStep grid visited queue row col (turnRight direction) heat 0 maxSteps
 
 
     let maxRow = grid.GetUpperBound(0)
@@ -109,5 +106,5 @@ let execute =
     let path = "day17/day17_input.txt"
     let lines = LocalHelper.ReadLines path |> List.ofSeq
     let matrix = parseInput lines
-    let result = travel matrix 1 3
+    let result = travel matrix 3
     result.Value
