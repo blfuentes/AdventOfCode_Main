9e11c339fd82f2b9ac5193e71e712c7c483bb365
blfuentes <blfuentes@mundo-r.com>
2024-11-02 20:51:19 +0100

    working on 201710

--- AdventOfCode_2017/day10/part01/day10_part01.fs
+++ AdventOfCode_2017/day10/part01/day10_part01.fs
@@ -1,8 +1,31 @@
 ﻿module day10_part01
 
+open AdventOfCode_2017.Modules
 
-
-let path = "day10/day10_input.txt"
+let rec processLengths (state: int array) (lengths: int list) (currentskip: int) (currentindex: int) =
+    match lengths with
+    | [] -> state
+    | length :: rest ->
+        let totake = length
+        let (takeleft, takeright) = 
+            if totake > (state.Length - currentindex) then 
+                (totake - (totake - (state.Length - currentindex)), totake - (state.Length - currentindex) - 1)
+            else
+                (0, totake)
+        let (lefttaken, righttaken) = (state |> Array.take(takeleft), state[currentindex..(currentindex + takeright - 1)])
+        let mid = state[..currentindex - 1] |> Array.skip(takeleft)
+        let ending = if takeleft = 0 then state[(currentindex+totake)..] else [||]
+        let newstate = 
+            if takeleft = 0 then
+                Array.concat([lefttaken |> Array.rev; mid; righttaken |> Array.rev; ending])
+            else
+                Array.concat([righttaken |> Array.rev; mid; lefttaken |> Array.rev; ending])
+        processLengths newstate rest (currentskip + 1) ((currentindex + length + currentskip) % state.Length)
 
 let execute =
-    0
+    let path = "day10/test_input_01.txt"
+    //let path = "day10/day10_input.txt"
+    let content = (LocalHelper.GetContentFromFile path).Split(", ") |> Array.map int |> List.ofArray
+    let size = 5
+    let circular = (Array.create size 0) |> Array.mapi(fun idx value -> idx)
+    processLengths circular content 0 0
--- AdventOfCode_2017/day10/part01/day10_part01.fsx
+++ AdventOfCode_2017/day10/part01/day10_part01.fsx
@@ -1,9 +1,30 @@
 ﻿#load @"../../../AdventOfCode_Utilities/Modules/Utilities.fs"
+#load @"../../Modules/LocalHelper.fs"
 
 open System
 open System.Collections.Generic
 
+open AdventOfCode_2017.Modules
 open AdventOfCode_Utilities
 
-//let path = "day10/test_input_01.txt"
-let path = "day10/day10_input.txt"
+let rec processLengths (state: int array) (lengths: int list) (currentskip: int) (currentindex: int) =
+    match lengths with
+    | [] -> state
+    | length :: rest ->
+        let totake = length + currentskip
+        let (takeleft, takeright) = 
+            if totake > (state.Length - currentindex) then 
+                (totake - (totake - (state.Length - currentindex)), totake - (state.Length - currentindex))
+            else
+                (0, totake)
+        let (lefttaken, righttaken) = (state |> Array.take(takeleft), state[currentindex..(currentindex + takeright - 1)])
+        let mid = state[..currentindex] |> Array.skip(takeleft)
+        let newstate = Array.concat([lefttaken; mid; righttaken])
+        processLengths newstate rest (currentskip + 1) ((currentindex + length)% length)
+
+let path = "day10/test_input_01.txt"
+//let path = "day10/day10_input.txt"
+let content = (LocalHelper.GetContentFromFile path).Split(", ") |> Array.map int |> List.ofArray
+let size = 5
+let circular = (Array.create size 0) |> Array.mapi(fun idx value -> idx)
+processLengths circular content 0 0
