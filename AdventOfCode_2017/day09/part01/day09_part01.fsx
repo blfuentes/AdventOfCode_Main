2f8503f2bf46f4faea9ed8dd8419ecfce33a505b
blfuentes <blfuentes@mundo-r.com>
2024-11-02 18:46:44 +0100

    finished 201709

--- AdventOfCode_2017/day09/part01/day09_part01.fsx
+++ AdventOfCode_2017/day09/part01/day09_part01.fsx
@@ -1,9 +1,31 @@
-﻿#load @"../../../AdventOfCode_Utilities/Modules/Utilities.fs"
+﻿
+#load @"../../../AdventOfCode_Utilities/Modules/Utilities.fs"
+#load @"../../Modules/LocalHelper.fs"
 
 open System
 open System.Collections.Generic
 
+open AdventOfCode_2017.Modules
 open AdventOfCode_Utilities
+
+type GarbageState = NotGarbage | Garbage | Cancelled
+type State = { level: int; state: GarbageState; score: int; garbage: int }
+
+let step current nextChar =
+    match (current.state, nextChar) with
+    | (Garbage, '!') -> { current with state = Cancelled }
+    | (Garbage, '>') -> { current with state = NotGarbage } 
+    | (Garbage, _)   -> { current with garbage = current.garbage + 1 }
+    | (Cancelled, _) | (NotGarbage, '<') -> { current with state = Garbage }
+    | (NotGarbage, '{') -> { current with level = current.level + 1 }
+    | (NotGarbage, '}') -> { current with level = current.level - 1; score = current.score + current.level }
+    | _ -> current;
+
+let solve = Seq.fold step { level=0; state=NotGarbage; score=0; garbage=0 }
+let solvePart1 = solve >> (fun state -> state.score)
 
 //let path = "day09/test_input_01.txt"
 let path = "day09/day09_input.txt"
+let input = LocalHelper.GetContentFromFile path
+
+solvePart1 input
--- AdventOfCode_2017/day09/part02/day09_part02.fs
+++ AdventOfCode_2017/day09/part02/day09_part02.fs
@@ -1,8 +1,24 @@
 ﻿module day09_part02
 
+open AdventOfCode_2017.Modules
 
+type GarbageState = NotGarbage | Garbage | Cancelled
+type State = { level: int; state: GarbageState; score: int; garbage: int }
 
-let path = "day09/day09_input.txt"
+let step current nextChar =
+    match (current.state, nextChar) with
+    | (Garbage, '!') -> { current with state = Cancelled }
+    | (Garbage, '>') -> { current with state = NotGarbage } 
+    | (Garbage, _)   -> { current with garbage = current.garbage + 1 }
+    | (Cancelled, _) | (NotGarbage, '<') -> { current with state = Garbage }
+    | (NotGarbage, '{') -> { current with level = current.level + 1 }
+    | (NotGarbage, '}') -> { current with level = current.level - 1; score = current.score + current.level }
+    | _ -> current;
+
+let folder = Seq.fold step { level=0; state=NotGarbage; score=0; garbage=0 }
+let solver = folder >> (fun state -> state.garbage)
 
 let execute =
-    0
+    let path = "day09/day09_input.txt"
+    let input = LocalHelper.GetContentFromFile path
+    solver input
