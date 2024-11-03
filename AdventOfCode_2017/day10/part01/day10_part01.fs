eca1f304f386b796b1224e880c6cf4dc473313e7
blfuentes <epiphonetodie@gmail.com>
2024-11-03 18:43:05 +0100

    refactor 201710

--- AdventOfCode_2017/day10/part01/day10_part01.fs
+++ AdventOfCode_2017/day10/part01/day10_part01.fs
@@ -2,22 +2,7 @@
 
 open AdventOfCode_2017.Modules
 
-type Node = {
-    Index: int;
-    Value: int;
-    NextIndex: int;
-}
-
-let getPart (state: Node array) (start: int) (count: int) =
-    let part = 
-        seq {
-            for idx in [start..start+count] do
-                yield state |> Array.find(fun s -> s.Index = idx % state.Length)
-        }
-    part |> Array.ofSeq
-        
-
-let rec processLengths (state: Node array) (lengths: int list) (currentskip: int) (currentindex: int) =
+let rec processLengths (state: int array) (lengths: int list) (currentskip: int) (currentindex: int) =
     match lengths with
     | [] -> state
     | totake :: rest ->
@@ -27,7 +12,7 @@ let rec processLengths (state: Node array) (lengths: int list) (currentskip: int
                     yield (state[idx % state.Length], idx % state.Length)
             } |> Array.ofSeq
 
-        Array.iteri2(fun idx el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
+        Array.iter2(fun el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
 
         processLengths state rest (currentskip + 1) ((currentindex + totake + currentskip) % state.Length)
 
@@ -36,7 +21,7 @@ let execute =
     let content = (LocalHelper.GetContentFromFile path).Split(",") |> Array.map(fun v ->  int v) |> List.ofArray
     let size = 256
     let circular = 
-        (Array.create size { Index = 0; Value = 0; NextIndex = 0 }) 
-        |> Array.mapi(fun idx value -> { Index = idx; Value = idx; NextIndex = (idx + 1) % size })
+        (Array.create size 0) 
+        |> Array.mapi(fun idx _ -> idx)
     processLengths circular content 0 0 |> ignore
-    circular |> Array.take(2) |> Array.map _.Value |> Array.reduce (*)
+    circular |> Array.take(2) |> Array.reduce (*)
--- AdventOfCode_2017/day10/part02/day10_part02.fs
+++ AdventOfCode_2017/day10/part02/day10_part02.fs
@@ -2,22 +2,7 @@
 
 open AdventOfCode_2017.Modules
 
-type Node = {
-    Index: int;
-    Value: int;
-    NextIndex: int;
-}
-
-let getPart (state: Node array) (start: int) (count: int) =
-    let part = 
-        seq {
-            for idx in [start..start+count] do
-                yield state |> Array.find(fun s -> s.Index = idx % state.Length)
-        }
-    part |> Array.ofSeq
-        
-
-let rec processLengths (roundsleft: int) (state: Node array) (initiallengths: int list) (lengths: int list) (currentskip: int) (currentindex: int) =
+let rec processLengths (roundsleft: int) (state: int array) (initiallengths: int list) (lengths: int list) (currentskip: int) (currentindex: int) =
     match lengths with
     | [] -> 
         if roundsleft > 0 then
@@ -31,7 +16,7 @@ let rec processLengths (roundsleft: int) (state: Node array) (initiallengths: in
                     yield (state[idx % state.Length], idx % state.Length)
             } |> Array.ofSeq
 
-        Array.iteri2(fun idx el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
+        Array.iter2(fun el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
 
         processLengths roundsleft state  initiallengths rest (currentskip + 1) ((currentindex + totake + currentskip) % state.Length)
 
@@ -39,16 +24,15 @@ let execute =
     let path = "day10/day10_input.txt"
     let text = (LocalHelper.GetContentFromFile path)
     let extra = "17, 31, 73, 47, 23".Split(", ") |> Array.map(fun v ->  int v) |> List.ofArray
-    let content = List.concat([text.ToCharArray() |> Array.map int |> List.ofArray;extra])
+    let content = List.concat([text.ToCharArray() |> Array.map int |> List.ofArray; extra])
     let size = 256
     let rounds = 64
     let circular = 
-        (Array.create size { Index = 0; Value = 0; NextIndex = 0 }) 
-        |> Array.mapi(fun idx value -> { Index = idx; Value = idx; NextIndex = (idx + 1) % size })
-    let cloned = content |> Array.ofList |> Array.copy |> List.ofArray
-    processLengths (rounds - 1)  circular cloned content 0 0 |> ignore
-    circular |> Array.take(2) |> Array.map _.Value |> Array.reduce (*) |> ignore
-    let chunks = circular |> Array.map _.Value |> Array.chunkBySize(16)
-    let reduced = chunks |> Array.map(fun c -> c |> Array.reduce (^^^))
-    let hexadecimals = reduced |> Array.map(fun v -> sprintf "%02X" v) |> Seq.ofArray
+        (Array.create size 0) 
+        |> Array.mapi(fun idx _ -> idx)
+    processLengths (rounds - 1)  circular content content 0 0 |> ignore
+    let hexadecimals = 
+        circular 
+            |> Array.chunkBySize(16)    
+            |> Array.map(fun c -> sprintf "%02X" (c |> Array.reduce (^^^)))
     (String.concat "" hexadecimals).ToLowerInvariant()
