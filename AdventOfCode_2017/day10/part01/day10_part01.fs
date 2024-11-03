36f9ccb84f36967f4ff379fb244dfccf8c074d79
blfuentes <epiphonetodie@gmail.com>
2024-11-03 15:54:19 +0100

    finished 20171001

--- AdventOfCode_2017/day10/part01/day10_part01.fs
+++ AdventOfCode_2017/day10/part01/day10_part01.fs
@@ -2,30 +2,41 @@
 
 open AdventOfCode_2017.Modules
 
-let rec processLengths (state: int array) (lengths: int list) (currentskip: int) (currentindex: int) =
+type Node = {
+    Index: int;
+    Value: int;
+    NextIndex: int;
+}
+
+let getPart (state: Node array) (start: int) (count: int) =
+    let part = 
+        seq {
+            for idx in [start..start+count] do
+                yield state |> Array.find(fun s -> s.Index = idx % state.Length)
+        }
+    part |> Array.ofSeq
+        
+
+let rec processLengths (state: Node array) (lengths: int list) (currentskip: int) (currentindex: int) =
     match lengths with
     | [] -> state
-    | length :: rest ->
-        let totake = length
-        let (takeleft, takeright) = 
-            if totake > (state.Length - currentindex) then 
-                (totake - (totake - (state.Length - currentindex)), totake - (state.Length - currentindex) - 1)
-            else
-                (0, totake)
-        let (lefttaken, righttaken) = (state |> Array.take(takeleft), state[currentindex..(currentindex + takeright - 1)])
-        let mid = state[..currentindex - 1] |> Array.skip(takeleft)
-        let ending = if takeleft = 0 then state[(currentindex+totake)..] else [||]
-        let newstate = 
-            if takeleft = 0 then
-                Array.concat([lefttaken |> Array.rev; mid; righttaken |> Array.rev; ending])
-            else
-                Array.concat([righttaken |> Array.rev; mid; lefttaken |> Array.rev; ending])
-        processLengths newstate rest (currentskip + 1) ((currentindex + length + currentskip) % state.Length)
+    | totake :: rest ->
+        let indexes = 
+            seq {
+                for idx in [currentindex..currentindex + totake - 1] do
+                    yield (state[idx % state.Length], idx % state.Length)
+            } |> Array.ofSeq
+
+        Array.iteri2(fun idx el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
+
+        processLengths state rest (currentskip + 1) ((currentindex + totake + currentskip) % state.Length)
 
 let execute =
-    let path = "day10/test_input_01.txt"
-    //let path = "day10/day10_input.txt"
-    let content = (LocalHelper.GetContentFromFile path).Split(", ") |> Array.map int |> List.ofArray
-    let size = 5
-    let circular = (Array.create size 0) |> Array.mapi(fun idx value -> idx)
-    processLengths circular content 0 0
+    let path = "day10/day10_input.txt"
+    let content = (LocalHelper.GetContentFromFile path).Split(",") |> Array.map(fun v ->  int v) |> List.ofArray
+    let size = 256
+    let circular = 
+        (Array.create size { Index = 0; Value = 0; NextIndex = 0 }) 
+        |> Array.mapi(fun idx value -> { Index = idx; Value = idx; NextIndex = (idx + 1) % size })
+    processLengths circular content 0 0 |> ignore
+    circular |> Array.take(2) |> Array.map _.Value |> Array.reduce (*)
--- AdventOfCode_2017/day10/part02/day10_part02.fs
+++ AdventOfCode_2017/day10/part02/day10_part02.fs
@@ -1,8 +1,43 @@
 ﻿module day10_part02
 
+open AdventOfCode_2017.Modules
 
+type Node = {
+    Index: int;
+    Value: int;
+    NextIndex: int;
+}
 
-let path = "day10/day10_input.txt"
+let getPart (state: Node array) (start: int) (count: int) =
+    let part = 
+        seq {
+            for idx in [start..start+count] do
+                yield state |> Array.find(fun s -> s.Index = idx % state.Length)
+        }
+    part |> Array.ofSeq
+        
+
+let rec processLengths (state: Node array) (lengths: int list) (currentskip: int) (currentindex: int) =
+    match lengths with
+    | [] -> state
+    | totake :: rest ->
+        let indexes = 
+            seq {
+                for idx in [currentindex..currentindex + totake - 1] do
+                    yield (state[idx % state.Length], idx % state.Length)
+            } |> Array.ofSeq
+
+        Array.iteri2(fun idx el rev -> state[snd el] <- fst rev) indexes (indexes |> Array.rev)
+
+        processLengths state rest (currentskip + 1) ((currentindex + totake + currentskip) % state.Length)
 
 let execute =
-    0
+    //let path = "day10/test_input_01.txt"
+    let path = "day10/day10_input.txt"
+    let content = (LocalHelper.GetContentFromFile path).Split(",") |> Array.map(fun v ->  int v) |> List.ofArray
+    let size = 256
+    let circular = 
+        (Array.create size { Index = 0; Value = 0; NextIndex = 0 }) 
+        |> Array.mapi(fun idx value -> { Index = idx; Value = idx; NextIndex = (idx + 1) % size })
+    processLengths circular content 0 0
+    circular |> Array.take(2) |> Array.map _.Value |> Array.reduce (*)
