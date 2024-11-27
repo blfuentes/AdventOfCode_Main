a487f1a92b00b0049f3b5bc49b6d6880b73c101f
blfuentes <epiphonetodie@gmail.com>
2024-11-27 15:55:06 +0100

    working on 201611

--- AdventOfCode_2016/day11/part01/day11_part01.fs
+++ AdventOfCode_2016/day11/part01/day11_part01.fs
@@ -1,8 +1,36 @@
 ﻿module day11_part01
 
+open System.Text.RegularExpressions
+open AdventOfCode_2016.Modules
 
+type Element =
+    | Generator of string
+    | Microchip of string
+    | Nothing
 
-let path = "day11/day11_input.txt"
+let buildElement (value: string) =
+    let parts = value.Split(" ")
+    match parts[1] with
+    | "generator" -> Generator(parts[0])
+    | "microchip" -> Microchip(parts[0].Split("-")[0])
+    | _ -> Nothing
+        
+
+let parseLine (line: string) =
+    let pattern = @"(\b[\w-]+\b)\s(?:generator|microchip|relevant)"
+    let match' = Regex.Matches(line, pattern)
+    let elements =
+        match'
+        |> Seq.map (fun m -> buildElement m.Value)
+        |> List.ofSeq
+    elements
+
+let parseContent (lines: string array) =
+    let elements = lines |> Array.mapi(fun idx line -> (idx, parseLine line))
+    elements
 
 let execute =
-    0
+    let path = "day11/day11_input.txt"
+    let content = LocalHelper.GetLinesFromFile path
+    let elementsbyfloor = parseContent content
+    elementsbyfloor.Length
