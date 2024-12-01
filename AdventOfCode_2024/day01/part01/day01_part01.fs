c32ddfed6cff0b06bfa5e10b27e1840b9e4b0033
blfuentes <blfuentes@mundo-r.com>
2024-12-01 18:34:46 +0100

    refactor parsing 202401

--- AdventOfCode_2024/day01/part01/day01_part01.fs
+++ AdventOfCode_2024/day01/part01/day01_part01.fs
@@ -4,15 +4,17 @@ open AdventOfCode_2024.Modules
 open System
 
 let parseContent (lines: string array) =
-    let pairs =
-        lines
-        |> Array.map(fun line -> 
-            ((int)(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]),
-                (int)(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1])))
-    (pairs |> Array.map fst), (pairs |> Array.map snd)
+    let (lefties, righties) =
+        (([], []), lines)
+        ||> Array.fold (fun (lefties, righties) line ->
+            match line.Split(" ", StringSplitOptions.RemoveEmptyEntries) with
+            | [|left; right|] -> (int left) :: lefties, (int right) :: righties
+            | _ -> failwith "error"
+        )
+    (lefties |> List.rev, righties |> List.rev)
 
 let execute =
     let path = "day01/day01_input.txt"
     let content = LocalHelper.GetLinesFromFile path
     let (lefts, rights) = parseContent content
-    Array.zip(lefts |> Array.sort) (rights |> Array.sort) |>  Array.sumBy (fun (a, b) -> abs (a - b))
+    List.zip(lefts |> List.sort) (rights |> List.sort) |>  List.sumBy (fun (a, b) -> abs (a - b))
--- AdventOfCode_2024/day01/part02/day01_part02.fs
+++ AdventOfCode_2024/day01/part02/day01_part02.fs
@@ -4,15 +4,17 @@ open AdventOfCode_2024.Modules
 open System
 
 let parseContent (lines: string array) =
-    let pairs =
-        lines
-        |> Array.map(fun line -> 
-            ((int)(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]),
-                (int)(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1])))
-    (pairs |> Array.map fst), (pairs |> Array.map snd)
+    let (lefties, righties) =
+        (([], []), lines)
+        ||> Array.fold (fun (lefties, righties) line ->
+            match line.Split(" ", StringSplitOptions.RemoveEmptyEntries) with
+            | [|left; right|] -> (int left) :: lefties, (int right) :: righties
+            | _ -> failwith "error"
+        )
+    (lefties |> List.rev, righties |> List.rev)
     
-let countTimes (element: int*int) (searchlist: (int*int) array) =
-    match searchlist |> Array.tryFind(fun a -> fst element = fst a) with
+let countTimes (element: int*int) (searchlist: (int*int) List) =
+    match searchlist |> List.tryFind(fun a -> fst element = fst a) with
     | Some(result) -> (fst element) * (snd element)  * (snd result)
     | None -> 0
 
@@ -20,7 +22,7 @@ let execute =
     let path = "day01/day01_input.txt"
     let content = LocalHelper.GetLinesFromFile path
     let (lefts, rights) = parseContent content
-    let groupLeft = lefts |> Array.countBy id
-    let groupRight = rights |> Array.countBy id
-    groupLeft |> Array.sumBy(fun l -> countTimes l groupRight)
+    let groupLeft = lefts |> List.countBy id
+    let groupRight = rights |> List.countBy id
+    groupLeft |> List.sumBy(fun l -> countTimes l groupRight)
     
