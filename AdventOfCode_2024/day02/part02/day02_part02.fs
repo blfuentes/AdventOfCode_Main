01a99b45370827502f9e384fa0c35ed59439a853
blfuentes <epiphonetodie@gmail.com>
2024-12-02 08:55:06 +0100

    refactor 202402

--- AdventOfCode_2024/day02/part02/day02_part02.fs
+++ AdventOfCode_2024/day02/part02/day02_part02.fs
@@ -14,19 +14,14 @@ let checkWithoutOne(elems: int array) =
     let areSafeInc(tocheck: int array) =
         tocheck
         |> Array.pairwise
-        |> Array.map (fun (a, b) -> b - a >= 1 && b - a <= 3)
-        |> Array.filter(fun e -> not e)
-        |> Array.length = 0
+        |> Array.forall (fun (a, b) -> b - a >= 1 && b - a <= 3)
     let areSafeDec(tocheck: int array) =
         tocheck
         |> Array.pairwise
-        |> Array.map (fun (a, b) -> a - b >= 1 && a - b <= 3)  
-        |> Array.filter(fun e -> not e)
-        |> Array.length = 0
+        |> Array.forall (fun (a, b) -> a - b >= 1 && a - b <= 3)  
 
-    elems :: (getExclusions elems)
-    |> List.filter (fun e -> (e |> areSafeInc) || (e |> areSafeDec))
-    |> List.length >= 1
+    getExclusions elems
+    |> List.exists (fun e -> (e |> areSafeInc) || (e |> areSafeDec))
 
 let execute =
     let path = "day02/day02_input.txt"
