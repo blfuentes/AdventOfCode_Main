b0c4d931ad4af34e31fa1c3b66393ec3b2c23093
blfuentes <blfuentes@mundo-r.com>
2024-12-02 20:13:07 +0100

    refactor 202402 and added helper function

--- AdventOfCode_2024/day02/part02/day02_part02.fs
+++ AdventOfCode_2024/day02/part02/day02_part02.fs
@@ -7,8 +7,9 @@ let parseContent (lines: string array) =
     lines
     |> Array.map(fun line -> line.Split(" ") |> Array.map int)
 
-let getExclusions (arr: 'a array) : 'a array list =
-    [for i in 0 .. arr.Length - 1 -> Array.append (arr.[0 .. i-1]) (arr.[i+1 ..])]
+let skipOnes collection =
+    [| 0..Array.length collection - 1 |]
+    |> Array.map(fun i -> Array.removeAt i collection)
 
 let areSafeInc(i: int array) should =
     i |> Array.pairwise |> Array.forall (fun (a,b) -> should a b)
@@ -22,8 +23,8 @@ let validDiff a b =
 let isSafe l = areSafeInc l validDiff || areSafeDec l validDiff
 
 let checkWithoutOne(elems: int array) =
-    getExclusions elems
-    |> List.exists isSafe 
+    skipOnes elems
+    |> Array.exists isSafe 
 
 let execute =
     let path = "day02/day02_input.txt"
