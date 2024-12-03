990aaaf113ae31592b69ca5a6bc500b7e8ded932
blfuentes <epiphonetodie@gmail.com>
2024-12-03 07:55:49 +0100

    finished 202403

--- AdventOfCode_2024/day03/part01/day03_part01.fs
+++ AdventOfCode_2024/day03/part01/day03_part01.fs
@@ -1,8 +1,36 @@
 ﻿module day03_part01
 
 open AdventOfCode_2024.Modules
+open System.Text.RegularExpressions
+
+let parsecontent(lines: string array) =
+    let parts =
+        lines 
+        |> Array.map(fun line ->
+            let regex = @"mul\(\d+,\d+\)"
+            let matches = Regex.Matches(line, regex)
+            match matches.Count with
+            | 0 -> [(0,0)]
+            | _ ->
+                let elems =
+                    seq {
+                        for m in matches do
+                            let tosplit = m.Value.Replace("mul(", "").Replace(")", "")
+                            yield ((int)(tosplit.Split(",")[0]), (int)(tosplit.Split(",")[1]))
+                    }
+                elems |> List.ofSeq
+        )
+    parts |> List.ofArray
+  
+let calculateLine(part: (int*int) list) =
+    part
+    |> List.map(fun (a, b) -> a*b)
+    |> List.reduce (+)
 
 let execute =
     let path = "day03/day03_input.txt"
-    let content = LocalHelper.GetContentFromFile path
-    0
+    let content = LocalHelper.GetLinesFromFile path
+    let elements = parsecontent content
+    elements
+    |> List.map calculateLine
+    |> List.sum
--- AdventOfCode_2024/day03/part02/day03_part02.fs
+++ AdventOfCode_2024/day03/part02/day03_part02.fs
@@ -1,8 +1,37 @@
 ﻿module day03_part02
 
 open AdventOfCode_2024.Modules
+open System.Text.RegularExpressions
+open System
+
+let solveline (input: string) =
+    let pattern = @"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+    let matches = Regex.Matches(input, pattern) |> Seq.cast<Match>
+
+    let folder (acc: int * bool) (m': Match) =
+        let result, shouldAdd = acc
+        let section = input.Substring(m'.Index, m'.Length)
+
+        match section with
+        | "do()" ->
+            result, true
+        | "don't()" ->
+            result, false
+        | _ when not shouldAdd ->
+            result, shouldAdd
+        | _ ->
+            let parts = section.Replace("mul(", "").Replace(")", "").Split(",")
+            let firstValue = int parts.[0]
+            let secondValue = int parts.[1]
+            result + (firstValue * secondValue), shouldAdd
+
+    let initialState = (0, true)
+    let finalResult, _ = Seq.fold folder initialState matches
+
+    finalResult
 
 let execute =
     let path = "day03/day03_input.txt"
     let content = LocalHelper.GetContentFromFile path
-    0
+    solveline content
+    
