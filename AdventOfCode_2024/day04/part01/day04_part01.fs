540a0ca7d8795a2bd4d2159d5d1b862f5accafe4
blfuentes <epiphonetodie@gmail.com>
2024-12-04 07:59:11 +0100

    finished 202404

--- AdventOfCode_2024/day04/part01/day04_part01.fs
+++ AdventOfCode_2024/day04/part01/day04_part01.fs
@@ -1,8 +1,79 @@
 ﻿module day04_part01
 
 open AdventOfCode_2024.Modules
+open System
+
+let parseContent (lines: string array) =
+    let map = Array2D.create lines.Length lines[0].Length ""
+    for rowIdx in [0..lines.Length - 1] do
+        for colIdx in [0..lines[0].Length - 1] do         
+            let row = lines[rowIdx].ToCharArray()
+            map[rowIdx, colIdx] <- (string)row[colIdx]
+    map
+
+let getDiagonals (matrix: string[,]) =
+    let rows = Array2D.length1 matrix
+    let cols = Array2D.length2 matrix
+
+    // diagonals from left to right
+    let leftToRightDiagonals =
+        [ for d in 0 .. rows + cols - 2 do
+            [ for i in 0 .. d do
+                let j = d - i
+                if i < rows && j < cols then yield matrix[i, j] ] ]
+
+    // diagonals from right to left
+    let rightToLeftDiagonals =
+        [ for d in 0 .. rows + cols - 2 do
+            [ for i in 0 .. d do
+                let j = cols - 1 - (d - i)
+                if i < rows && j >= 0 then yield matrix[i, j] ] ]
+
+    leftToRightDiagonals @ rightToLeftDiagonals
+
+let getRow (matrix: string[,]) row = 
+    let cols = Array2D.length2 matrix 
+    [| for col in 0 .. cols - 1 do yield matrix[row, col] |]
+
+let getCol (matrix: string[,]) col = 
+    let rows = Array2D.length1 matrix 
+    [| for row in 0 .. rows - 1 do yield matrix[row, col] |]
+
+let countTimesOverlapped (pattern: string) (input: string) =
+    let rec countFromIndex index count =
+        match input.IndexOf(pattern, index, StringComparison.OrdinalIgnoreCase) with
+        | -1 -> count
+        | i -> countFromIndex (i + 1) (count + 1)
+
+    countFromIndex 0 0
+
+let countXmas(map: string [,]) =
+    let maxRows = map.GetLength(0)
+    let maxCols = map.GetLength(1)
+    let mutable count = 0
+    for rowIdx in [0..maxRows-1] do
+        let row = String.concat "" (getRow  map rowIdx) 
+        count <- count + countTimesOverlapped "XMAS" row
+        count <- count + countTimesOverlapped "SAMX" row
+    for colIdx in [0..maxCols-1] do
+        let col = String.concat "" (getCol  map colIdx) 
+        count <- count + countTimesOverlapped "XMAS" col
+        count <- count + countTimesOverlapped "SAMX" col    
+    let diagonals = getDiagonals map |> List.map(fun d -> String.concat "" d)
+    let foundIndDiagonals = 
+        diagonals
+        |> List.map(fun d -> 
+            (countTimesOverlapped "XMAS" d) +
+            (countTimesOverlapped "SAMX" d))
+        |> List.sum
+
+    count <- count + foundIndDiagonals
+
+    count
 
 let execute =
     let path = "day04/day04_input.txt"
-    let content = LocalHelper.GetContentFromFile path
-    0
+    //let path = "day04/test_input_01.txt"
+    let content = LocalHelper.GetLinesFromFile path
+    let map = parseContent content
+    countXmas map
--- AdventOfCode_2024/day04/part02/day04_part02.fs
+++ AdventOfCode_2024/day04/part02/day04_part02.fs
@@ -2,7 +2,38 @@
 
 open AdventOfCode_2024.Modules
 
+let parseContent (lines: string array) =
+    let map = Array2D.create lines.Length lines[0].Length ""
+    for rowIdx in [0..lines.Length - 1] do
+        for colIdx in [0..lines[0].Length - 1] do         
+            let row = lines[rowIdx].ToCharArray()
+            map[rowIdx, colIdx] <- (string)row[colIdx]
+    map
+
+let buildSubArray (matrix: string[,]) =
+    let rows = Array2D.length1 matrix
+    let cols = Array2D.length2 matrix
+
+    [ for i in 0 .. rows - 3 do
+        for j in 0 .. cols - 3 do
+            yield
+                Array2D.init 3 3 (fun x y -> matrix[i + x, j + y]) ]
+    |> List.filter(fun m -> m[1, 1] = "A")
+
+let hasMASMAS (map: string [,]) =
+    let middle = map[1,1] = "A"
+    let lefttop = (map[0,0] = "M" && map[2,2] = "S") || (map[0,0] = "S" && map[2,2] = "M")
+    let righttop = (map[0,2] = "M" && map[2,0] = "S") || (map[0,2] = "S" && map[2,0] = "M")
+
+    if middle && lefttop && righttop then 1 else 0
+
+let countXmas(map: string [,]) =
+    buildSubArray map
+    |> List.map(fun m -> hasMASMAS m)
+    |> List.sum
+
 let execute =
     let path = "day04/day04_input.txt"
-    let content = LocalHelper.GetContentFromFile path
-    0
+    let content = LocalHelper.GetLinesFromFile path
+    let map = parseContent content
+    countXmas map
