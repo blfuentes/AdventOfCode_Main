b5cd8a894f23b2885e8d204541198193c4adb2f0
blfuentes <epiphonetodie@gmail.com>
2024-12-05 06:59:00 +0100

    finished 202405

--- AdventOfCode_2024/day05/part01/day05_part01.fs
+++ AdventOfCode_2024/day05/part01/day05_part01.fs
@@ -1,8 +1,35 @@
 ﻿module day05_part01
 
 open AdventOfCode_2024.Modules
+open AdventOfCode_Utilities
+
+let parseContent(string: string list) =
+    let parts = Utilities.getGroupsOnSeparator string ""
+    let orders = parts[0] |> List.map(fun l -> ((int)(l.Split("|")[0]), (int)(l.Split("|")[1])))
+    let chekers = parts[1] |> List.map(fun l -> l.Split(",") |> Array.map int |> List.ofArray)
+    (orders, chekers)
+
+let inOrder(number: int) (tocheck: int list) (pages: (int*int) list) =
+    tocheck
+    |> List.forall(fun c ->
+        match pages |> List.tryFind(fun (f, t) -> f = number && c = t) with
+        | Some(_) -> true
+        | None -> false
+    )
+
+let rec checkerInOrder(pairs: int list)(pages: (int*int) list) =
+    match pairs with
+    | [] -> true
+    | head::tail ->
+        if inOrder head tail pages then
+            checkerInOrder tail pages
+        else
+            false
 
 let execute =
     let path = "day05/day05_input.txt"
-    let content = LocalHelper.GetContentFromFile path
-    0
+    let content = LocalHelper.GetLinesFromFile path |> List.ofArray
+    let (orders, checkers) = parseContent content
+    checkers
+    |> List.filter(fun o -> checkerInOrder o orders)
+    |> List.sumBy(fun l -> l.Item(l.Length / 2))
