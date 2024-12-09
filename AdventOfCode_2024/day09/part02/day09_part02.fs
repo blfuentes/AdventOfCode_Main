0082faba6a719026f0f035acb4298a33435868f3
blfuentes <epiphonetodie@gmail.com>
2024-12-09 13:24:13 +0100

    refactor 202409

--- AdventOfCode_2024/day09/part02/day09_part02.fs
+++ AdventOfCode_2024/day09/part02/day09_part02.fs
@@ -3,63 +3,85 @@
 open AdventOfCode_2024.Modules
 
 let parseContent(line: string) =
-    line.ToCharArray() |> Array.map (fun v -> System.Int32.Parse((string)v))
+    let numFiles = (line.Length + 1) / 2
+    let files = Array.zeroCreate numFiles
+    let freefiles = Array.zeroCreate (numFiles - 1)
+    for fIdx in 0..numFiles-1 do
+        files[fIdx] <- (int)(line[2*fIdx].ToString())
+        if fIdx < numFiles - 1 then
+            freefiles[fIdx] <- (int)(line[2*fIdx+1].ToString())
+    (files, freefiles)
 
-let buildblocks (input: int array) =
-    let builder = new System.Text.StringBuilder()
-    let mutable currentVIdx = 0
+let buildBlocksAndPositions (numOfBlocks: int) (numOfFiles: int) (files: int array) (freefiles: int array) =
+    let blocks = Array.create numOfBlocks -1
+    let positions = Array.zeroCreate numOfFiles
+    let mutable block = 0
 
-    input
-    |> Array.mapi(fun idx v ->
-        let replica =
-            if idx = 0 then
-                "0"
+    for fIdx in 0 .. (numOfFiles - 1) do
+        positions[fIdx] <- block
+        for b in block .. block + files[fIdx] - 1 do
+            blocks[b] <- fIdx
+        block <- block + files[fIdx]
+        if fIdx < numOfFiles - 1 then
+            block <- block + freefiles[fIdx]
+    (blocks, positions)    
+
+let countFreeBlocks (blocks: int[]) from =
+    let mutable freeblocks = 0
+    let mutable isdone = false
+    
+    while not isdone do
+        if from + 1 >= blocks.Length || blocks[from + freeblocks] > -1 then
+            isdone <- true
+        else
+            freeblocks <- freeblocks + 1
+    
+    freeblocks
+
+let gapOfSizeAvailable (blocks: int[]) size position =
+    let mutable block = 0
+    let mutable finished = false
+    let mutable result : int option = None
+
+    while not finished do
+        if block >= position then
+            finished <- true
+        elif blocks[block] > -1 then
+            block <- block + 1
+        else
+            let numFree = countFreeBlocks blocks block
+            if numFree >= size then
+                result <- Some block
+                finished <- true
             else
-                if idx % 2 = 0 then 
-                    currentVIdx.ToString()                     
-                else "."
-        if replica <> "." then currentVIdx <- currentVIdx + 1
-        Array.create v replica
-    )
-
-let splitParts (inputArray: string array array) =
-    inputArray
-    |> Array.collect(fun subarray ->
-        subarray |> Array.groupBy id |> Array.map snd
-    )
-
-let processblocks (input: string array array) =
-    let mutable touse = splitParts input
-    let mutable blockIdx = touse.Length - 1
-    let mutable gapIdx = 0
-    while blockIdx > gapIdx do
-        if blockIdx % 500 = 0 then
-            printfn "block %d of %d" blockIdx input.Length
-        while blockIdx > 0  && touse[blockIdx][0] = "." do
-            blockIdx <- blockIdx - 1
-        let blocksize = touse[blockIdx].Length
-        while gapIdx < blockIdx && (not ((touse[gapIdx][0]).Equals(("."))) || touse[gapIdx].Length < blocksize) do
-            gapIdx <- gapIdx + 1
-
-        if blockIdx > 0 && blockIdx > gapIdx then
-            let found = touse[blockIdx]
-            for sIdx in [0..found.Length-1] do
-                touse[gapIdx][sIdx] <- found[sIdx]
-                touse[blockIdx][sIdx] <- "."
-            touse <- splitParts touse
-            gapIdx <- 0
-        if gapIdx = blockIdx then
-            blockIdx <- blockIdx - 1
-            gapIdx <- 0
-    touse |> Array.concat
+                block <- block + numFree
+    result
 
 let execute() =
     let path = "day09/day09_input.txt"
-    //let path = "day09/test_input_01.txt"
     let content = LocalHelper.GetContentFromFile path
-    let block = buildblocks (parseContent content)
-    let processed = processblocks block
-    //printfn "%s" (String.concat "" processed)
-    processed
-    |> Array.mapi(fun idx v -> if v = "." then 0I else (bigint(idx) * bigint.Parse(v)))
+    let numFiles = (content.Length + 1) / 2
+
+    let (files, freefiles) = parseContent content
+    
+    let allfilengths = Array.sum files
+    
+    let blocks = allfilengths + Array.sum freefiles
+    
+    let (initBlocks, fPositions) = 
+        buildBlocksAndPositions blocks numFiles files freefiles
+
+    let blocks = Array.copy initBlocks
+    for fIdx in (numFiles - 1) .. -1 .. 1 do
+        let requiredGapSize = files[fIdx]
+        let filePosition = fPositions[fIdx]
+        match gapOfSizeAvailable blocks requiredGapSize filePosition with
+        | None -> ()
+        | Some firstFree ->
+            for gapIdx in 0 .. requiredGapSize - 1 do
+                blocks[firstFree + gapIdx] <- fIdx
+                blocks[filePosition + gapIdx] <- -1
+
+    blocks
+    |> Array.mapi(fun idx v -> if v = -1 then 0I else (bigint(idx) * bigint.Parse(v.ToString())))
     |> Array.sum
