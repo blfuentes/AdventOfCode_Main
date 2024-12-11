ad5cf3becd08e1edcad1d5bccd7488cd181f27ab
blfuentes <epiphonetodie@gmail.com>
2024-12-11 09:01:20 +0100

    refactor 202411

--- AdventOfCode_2024/day11/part02/day11_part02.fs
+++ AdventOfCode_2024/day11/part02/day11_part02.fs
@@ -3,16 +3,19 @@
 open AdventOfCode_2024.Modules
 open System.Collections.Generic
 
+let InsertOrUpdate(collection: Dictionary<int64, int64>) (key: int64) (value: int64) =
+    collection[key] <- 
+        if collection.ContainsKey(key) then
+            collection[key] + value
+        else
+            value
+
 let parseContent (lines: string) =
     let data = Dictionary<int64, int64>()
     lines.Split(" ")
     |> Array.iter (fun s ->
         let key = int64 s
-        data[key] <-
-            if data.ContainsKey(key) then
-                data.[key] + 1L
-            else
-                1L
+        InsertOrUpdate data key 1L
     )
     data
 
@@ -28,43 +31,26 @@ let runMurations (stonesdata: Dictionary<int64, int64>) (step: int) =
         
             match stonekey with
             | sk when sk = 0L ->
-                newStoneData[1L] <- 
-                    if newStoneData.ContainsKey(1) then 
-                        newStoneData[1L] + stoneData 
-                    else 
-                        stoneData
+                InsertOrUpdate newStoneData 1L stoneData
             | sk when length % 2 = 0 ->
-                let mid = sk.ToString().Length / 2
+                let mid = length / 2
                 let left, right = (
                     System.Int64.Parse(sk.ToString()[0..mid-1])
                     , System.Int64.Parse(sk.ToString()[mid..]))
-                newStoneData[left] <- 
-                    if newStoneData.ContainsKey(left) then
-                        newStoneData[left] + stoneData
-                    else
-                        stoneData
-                newStoneData[right] <- 
-                    if newStoneData.ContainsKey(right) then
-                        newStoneData[right] + stoneData
-                    else
-                        stoneData
+                InsertOrUpdate newStoneData left stoneData
+                InsertOrUpdate newStoneData right stoneData
             | _ ->
                 let newStoneKey = stonekey * 2024L
-                newStoneData[newStoneKey] <-
-                    if newStoneData.ContainsKey(newStoneKey) then
-                        newStoneData[newStoneKey] + stoneData
-                    else
-                        stoneData
+                InsertOrUpdate newStoneData newStoneKey stoneData
         currentStoneData <- newStoneData
     )
         
     currentStoneData
 
-
 let execute() =
     let path = "day11/day11_input.txt"
     let content = LocalHelper.GetContentFromFile path
     let stones = parseContent content
-    let mutatedValues = runMurations stones 75
+    let stoneQuantityData = runMurations stones 75
 
-    mutatedValues.Values |> Seq.sum
+    stoneQuantityData.Values |> Seq.sum
