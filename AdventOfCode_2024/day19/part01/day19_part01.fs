8204a8112a98189dc8e62c9a9eccb9491a0f8665
blfuentes <epiphonetodie@gmail.com>
2024-12-19 09:57:49 +0100

    refactor 202419

--- AdventOfCode_2024/day19/part01/day19_part01.fs
+++ AdventOfCode_2024/day19/part01/day19_part01.fs
@@ -4,25 +4,26 @@ open AdventOfCode_2024.Modules
 open System.Collections.Generic
 
 let parseContent(lines: string) =
-    let patterns = (lines.Split("\r\n\r\n")[0]).Split(", ")
+    let patterns = (lines.Split("\r\n\r\n")[0]).Split(", ")|> Set.ofArray
     let designs = (lines.Split("\r\n\r\n")[1]).Split("\r\n")
     (patterns, designs)
 
-let checkIfValid (design: string) (patterns: Set<string>) maxSize (memo: Dictionary<string, bool>) =   
+let checkIfValid (design: string) (patterns: Set<string>) maxSize (memo: Dictionary<string, bool>) = 
     let rec canBeDone (patterns: Set<string>) designpart =
-        if memo.ContainsKey designpart then
-            memo[designpart]
-        else
-            if designpart = "" then true
-            else
+        match memo.TryGetValue(designpart) with
+        | found, value when found -> value
+        | _ ->
+           match designpart with
+           | "" -> true
+           | _ ->
                 let minlength = min maxSize designpart.Length
-                let mutable found = false
-                for l in 0..minlength do
-                    let subpart = designpart.Substring(0, l)
-                    if Set.contains subpart patterns then
-                        if canBeDone patterns (designpart.Substring(subpart.Length)) then
-                            found <- true
 
+                let found = 
+                    [0..minlength]
+                    |> Seq.map(fun l -> designpart.Substring(0, l))
+                    |> Seq.filter(fun subpart -> Set.contains subpart patterns)
+                    |> Seq.exists(fun subpart -> canBeDone patterns (designpart.Substring(subpart.Length)))
+                
                 memo.Add(designpart, found)
                 found
 
@@ -30,12 +31,16 @@ let checkIfValid (design: string) (patterns: Set<string>) maxSize (memo: Diction
 
 let execute() =
     let path = "day19/day19_input.txt"
+
     let content = LocalHelper.GetContentFromFile path
     let (patterns, designs) = parseContent content
-    let maxPatternSize = ((patterns |> Array.sortByDescending _.Length)[0]).Length
+    let maxPatternSize = 
+        patterns
+        |> Seq.maxBy (fun pattern -> pattern.Length)
+        |> fun pattern -> pattern.Length
+
     let memo = Dictionary<string, bool>()
-    let mutable found = 0
     designs
     |> Array.sumBy(fun d ->
-        if checkIfValid d (patterns |> Set.ofArray) maxPatternSize memo then 1 else 0
+        if checkIfValid d patterns maxPatternSize memo then 1 else 0
     )
--- AdventOfCode_2024/day19/part02/day19_part02.fs
+++ AdventOfCode_2024/day19/part02/day19_part02.fs
@@ -4,25 +4,27 @@ open AdventOfCode_2024.Modules
 open System.Collections.Generic
 
 let parseContent(lines: string) =
-    let patterns = (lines.Split("\r\n\r\n")[0]).Split(", ")
+    let patterns = (lines.Split("\r\n\r\n")[0]).Split(", ") |> Set.ofArray
     let designs = (lines.Split("\r\n\r\n")[1]).Split("\r\n")
     (patterns, designs)
 
 let checkIfValid (design: string) (patterns: Set<string>) maxSize (memo: Dictionary<string, int64>) =   
     let rec canBeDone (patterns: Set<string>) foundSoFar designpart =
-        if memo.ContainsKey designpart then
-            memo[designpart]
-        else
-            if designpart = "" then foundSoFar + 1L
-            else
+        match memo.TryGetValue(designpart) with
+        | found, value when found -> value
+        | _ ->
+           match designpart with
+           | "" -> foundSoFar + 1L
+           | _ ->
                 let minlength = min maxSize designpart.Length
-                let mutable numOfFounds = 0L
-                for l in 0..minlength do
-                    let subpart = designpart.Substring(0, l)
-                    if Set.contains subpart patterns then
-                        numOfFounds <- numOfFounds + canBeDone patterns foundSoFar (designpart.Substring(subpart.Length))
 
-                memo.Add(designpart, numOfFounds) |> ignore
+                let numOfFounds = 
+                    [0..minlength]
+                    |> Seq.map(fun l -> designpart.Substring(0, l))
+                    |> Seq.filter(fun subpart -> Set.contains subpart patterns)
+                    |> Seq.sumBy(fun subpart -> canBeDone patterns foundSoFar (designpart.Substring(subpart.Length)))
+            
+                memo.Add(designpart, numOfFounds)
                 numOfFounds
 
     canBeDone patterns 0 design
@@ -31,9 +33,13 @@ let execute() =
     let path = "day19/day19_input.txt"
     let content = LocalHelper.GetContentFromFile path
     let (patterns, designs) = parseContent content
-    let maxPatternSize = ((patterns |> Array.sortByDescending _.Length)[0]).Length
+    let maxPatternSize = 
+        patterns
+        |> Seq.maxBy (fun pattern -> pattern.Length)
+        |> fun pattern -> pattern.Length
     let memo = Dictionary<string, int64>()
+
     designs
     |> Array.sumBy(fun d ->
-        checkIfValid d (patterns |> Set.ofArray) maxPatternSize memo
+        checkIfValid d patterns maxPatternSize memo
     )
