723ba1c109539832a599e17ae66a852d2d58ace2
blfuentes <blfuentes@mundo-r.com>
2024-12-26 09:25:12 +0100

    finished 201517

--- AdventOfCode_2015/day17/part01/day17_part01.fs
+++ AdventOfCode_2015/day17/part01/day17_part01.fs
@@ -1,4 +1,34 @@
 ﻿module day17_part01
 
+open AdventOfCode_2015.Modules
+
+let parseContent(lines: string array) =
+    lines |> Array.map int
+
+let findCombinationsMemoized target numbers =
+    let memo = System.Collections.Generic.Dictionary<_, _>()
+    let rec inner target numbers =
+        match target, numbers with
+        | 0, _ -> [ [] ]
+        | _, [] -> []
+        | _, x :: xs ->
+            if x > target then
+                inner target xs
+            else
+                let key = (target, numbers)
+                if memo.ContainsKey(key) then
+                    memo[key]
+                else
+                    let withX = inner (target - x) xs |> List.map (fun subset -> x :: subset)
+                    let withoutX = inner target xs
+                    let result = withX @ withoutX
+                    memo[key] <- result
+                    result
+
+    inner target numbers
+    
 let execute =
-    0
+    let input = "./day17/day17_input.txt"
+    let content = LocalHelper.GetLinesFromFile input
+    let containers = parseContent content |> List.ofArray
+    (findCombinationsMemoized 150 containers).Length
--- AdventOfCode_2015/day17/part02/day17_part02.fs
+++ AdventOfCode_2015/day17/part02/day17_part02.fs
@@ -1,4 +1,42 @@
 ﻿module day17_part02
 
+open AdventOfCode_2015.Modules
+
+let parseContent(lines: string array) =
+    lines |> Array.map int
+
+let findSmallestCombinationsMemoized target numbers =
+    let memo = System.Collections.Generic.Dictionary<_, _>()
+    let rec inner target numbers =
+        match target, numbers with
+        | 0, _ -> [ [] ]
+        | _, [] -> []
+        | _, x :: xs ->
+            if x > target then
+                inner target xs
+            else
+                let key = (target, numbers)
+                if memo.ContainsKey(key) then
+                    memo[key]
+                else
+                    let withX = inner (target - x) xs |> List.map (fun subset -> x :: subset)
+                    let withoutX = inner target xs
+                    let result = withX @ withoutX
+                    memo[key] <- result
+                    result
+
+    let possibleCombinations = inner target numbers
+    let (_, smallest) =
+        possibleCombinations
+        |> List.groupBy _.Length
+        |> List.sort
+        |> List.head
+    smallest.Length
+    
+    
 let execute =
-    0
+    let input = "./day17/day17_input.txt"
+    let content = LocalHelper.GetLinesFromFile input
+    let containers = parseContent content |> List.ofArray
+    let possibleways = findSmallestCombinationsMemoized 150 containers
+    possibleways
