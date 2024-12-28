4ff7caac24b377940b4d9dd0f62c1f6c65b11c27
blfuentes <blfuentes@mundo-r.com>
2024-12-28 20:35:40 +0100

    finished 201616

--- AdventOfCode_2016/day16/part01/day16_part01.fs
+++ AdventOfCode_2016/day16/part01/day16_part01.fs
@@ -1,8 +1,36 @@
 ﻿module day16_part01
 
+open AdventOfCode_2016.Modules
 
+let dragonCurve(input: char array) =
+    let a = input
+    let b = a
+    let reversed = b |> Array.rev
+    let replaced = reversed |> Array.map(fun c -> if c = '1' then '0' else '1')
+    Array.concat [a; [|'0'|]; replaced]
 
-let path = "day16/day16_input.txt"
+let checksum(input: char array) =
+    let mutable repeat = true
+    let mutable checksum = input
+    while repeat do
+        checksum <-
+            checksum
+            |> Array.chunkBySize 2
+            |> Array.map(fun r -> if r[0] = r[1] then '1' else '0')
+        if checksum.Length % 2 <> 0 then
+            repeat <- false
+    checksum
+        
+let fillDisk(size: int) (input: char array) =
+    let mutable dragonc = dragonCurve input
+    while dragonc.Length < size do
+        dragonc <- dragonCurve dragonc
+    checksum (dragonc |> Array.take(size))
+
 
 let execute =
-    0
+    let path = "day16/day16_input.txt"
+    let content = (LocalHelper.GetContentFromFile path).ToCharArray()
+    let size = 272
+
+    String.concat "" ((fillDisk size content)|> Array.map string)
