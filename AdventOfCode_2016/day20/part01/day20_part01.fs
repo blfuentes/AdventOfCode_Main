c0c8abce5d9baeac700bd509365fb620b823936f
blfuentes <epiphonetodie@gmail.com>
2024-12-30 09:09:38 +0100

    finished 201620

--- AdventOfCode_2016/day20/part01/day20_part01.fs
+++ AdventOfCode_2016/day20/part01/day20_part01.fs
@@ -1,8 +1,26 @@
 ﻿module day20_part01
 
+open AdventOfCode_2016.Modules
 
+let parseContent(lines: string array) =
+    lines
+    |> Array.map(fun l ->
+        ((int64)(l.Split("-")[0]), (int64)(l.Split("-")[1]))
+    )
 
-let path = "day20/day20_input.txt"
+let findFirstAvailable(blockedRanges: (int64*int64) array) =
+    let mutable lowest = 0L
+    let sorted = blockedRanges |> Array.sortBy(fun (f,t) -> f)
+    
+    for idx in [0..sorted.Length-1] do
+        let (bottom, top) = sorted[idx]
+        if lowest >= bottom && lowest <= top then
+            lowest <- top + 1L    
+    lowest
 
 let execute =
-    0
+    let path = "day20/day20_input.txt"
+    let content = LocalHelper.GetLinesFromFile path
+
+    let blockedRanges = parseContent content
+    findFirstAvailable blockedRanges
--- AdventOfCode_2016/day20/part02/day20_part02.fs
+++ AdventOfCode_2016/day20/part02/day20_part02.fs
@@ -1,8 +1,33 @@
 ﻿module day20_part02
 
+open AdventOfCode_2016.Modules
 
+let parseContent(lines: string array) =
+    lines
+    |> Array.map(fun l ->
+        ((int64)(l.Split("-")[0]), (int64)(l.Split("-")[1]))
+    )
 
-let path = "day20/day20_input.txt"
+let findAllValidIps(maxvalue: int64) (blockedRanges: (int64*int64) array) =
+    let isfound (value: int64) =
+        blockedRanges
+        |> Array.exists(fun (f, t) -> value >= f && value <= t || value > maxvalue) 
+
+    let possible = blockedRanges |> Array.map(fun (f, t) -> t + 1L)
+    let expected = possible |> Array.filter(fun v -> not (isfound v))
+    let mutable counter = 0L
+    for ip in expected do
+        let mutable ip' = ip
+        while not(isfound ip') do
+            counter <- counter + 1L
+            ip' <- ip' + 1L
+    counter
+
 
 let execute =
-    0
+    let path = "day20/day20_input.txt"
+    let content = LocalHelper.GetLinesFromFile path
+
+    let blockedRanges = parseContent content
+    let maxvalue = 4294967295L
+    findAllValidIps maxvalue blockedRanges
