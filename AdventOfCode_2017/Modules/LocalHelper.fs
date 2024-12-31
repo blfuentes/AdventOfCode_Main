95c31a783a0b8407ed3cd36765dd9a6118fa203c
blfuentes <blfuentes@mundo-r.com>
2024-12-31 19:08:24 +0100

    finished 20171401

--- AdventOfCode_2017/Modules/LocalHelper.fs
+++ AdventOfCode_2017/Modules/LocalHelper.fs
@@ -11,3 +11,34 @@ module LocalHelper =
 
     let ReadLines(path: string) =
         File.ReadLines(__SOURCE_DIRECTORY__ + @"../../" + path)
+
+    let knotHasher(text: string) =
+        let rec processLengths (roundsleft: int) (state: int array) (initiallengths: int list) (lengths: int list) (currentskip: int) (currentindex: int) =
+            match lengths with
+            | [] -> 
+                if roundsleft > 0 then
+                    processLengths (roundsleft - 1) state initiallengths initiallengths currentskip currentindex
+                else
+                    state
+            | totake :: rest ->
+                let indexes = 
+                    seq {
+                        for idx in [currentindex..currentindex + totake - 1] do
+                            yield (state[idx % state.Length], idx % state.Length)
+                    }
+
+                Seq.iter2(fun el rev -> state[snd el] <- fst rev) indexes (indexes |> Seq.rev)
+
+                processLengths roundsleft state  initiallengths rest (currentskip + 1) ((currentindex + totake + currentskip) % state.Length) 
+
+        let extra = "17, 31, 73, 47, 23".Split(", ") |> Array.map int |> List.ofArray
+        let content = List.concat([text.ToCharArray() |> Array.map int |> List.ofArray; extra])
+        let size = 256
+        let rounds = 64
+        let circular = [|0 .. size - 1|]
+        processLengths (rounds - 1)  circular content content 0 0 |> ignore
+        let hexadecimals = 
+            circular 
+                |> Array.chunkBySize(16)    
+                |> Array.map(fun c -> sprintf "%02X" (c |> Array.reduce (^^^)))
+        (String.concat "" hexadecimals).ToLowerInvariant()
--- AdventOfCode_2017/day14/part01/day14_part01.fs
+++ AdventOfCode_2017/day14/part01/day14_part01.fs
@@ -1,8 +1,32 @@
 ﻿module day14_part01
 
+open AdventOfCode_2017.Modules
 
+let hexToBinary hexDigit =
+    let hexCharToInt c =
+        match c with
+        | '0' -> 0 | '1' -> 1 | '2' -> 2 | '3' -> 3
+        | '4' -> 4 | '5' -> 5 | '6' -> 6 | '7' -> 7
+        | '8' -> 8 | '9' -> 9
+        | 'a' | 'A' -> 10 | 'b' | 'B' -> 11
+        | 'c' | 'C' -> 12 | 'd' | 'D' -> 13
+        | 'e' | 'E' -> 14 | 'f' | 'F' -> 15
+        | _ -> failwith "Invalid hexadecimal digit"
+    let value = hexCharToInt hexDigit
+    System.Convert.ToString(value, 2).PadLeft(4, '0')
 
-let path = "day14/day14_input.txt"
+let buildRows(input: string) =
+    let sum =
+        [0..127]
+        |> List.sumBy(fun row ->
+            let knothash = String.concat "" ((LocalHelper.knotHasher $"{input}-{row}").ToCharArray() |> Array.map(fun c -> hexToBinary c))
+            let hashedrow = knothash.ToCharArray() |> Array.map(fun c -> if c = '1' then 1 else 0)
+            hashedrow |> Array.sum
+        )
+    sum
 
 let execute() =
-    0
+    let path = "day14/day14_input.txt"
+    //let path = "day14/test_input_14.txt"
+    let content = LocalHelper.GetContentFromFile path
+    buildRows content
