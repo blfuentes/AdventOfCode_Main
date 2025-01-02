380fea4c012bf74e54fb332df324543cdf90c56f
blfuentes <epiphonetodie@gmail.com>
2025-01-02 12:43:51 +0100

    finished 201717

--- AdventOfCode_2017/day17/part01/day17_part01.fs
+++ AdventOfCode_2017/day17/part01/day17_part01.fs
@@ -1,8 +1,28 @@
 ﻿module day17_part01
 
+open AdventOfCode_2017.Modules
 
-
-let path = "day17/day17_input.txt"
+let startInsertions (buffer: int array) (numsteps: int) (stopat: int) =
+    let rec insertElement (index: int) (currentvalue: int) (currentBuffer: int array) =
+        if currentvalue = stopat+1 then
+            currentBuffer[index+1]
+        else
+            let newindex = (index + numsteps) % currentBuffer.Length
+            match newindex = currentBuffer.Length-1 with
+            | true ->
+                let newbuffer = Array.concat [| currentBuffer; [| currentvalue |] |]
+                insertElement (newindex+1) (currentvalue+1) newbuffer
+            | false ->
+                let leftpart = Array.sub currentBuffer 0 (newindex + 1)
+                let rightpart = Array.sub currentBuffer (newindex + 1) (currentBuffer.Length - newindex - 1)
+                let newbuffer = Array.concat [| leftpart; [| currentvalue |]; rightpart |]
+                insertElement (newindex+1) (currentvalue+1) newbuffer
+        
+    insertElement 0 1 buffer
 
 let execute() =
-    0
+    let path = "day17/day17_input.txt"
+    let steps = LocalHelper.GetContentFromFile path |> int
+
+    let buffer = [| 0 |]
+    startInsertions buffer steps 2017
--- AdventOfCode_2017/day17/part02/day17_part02.fs
+++ AdventOfCode_2017/day17/part02/day17_part02.fs
@@ -1,8 +1,22 @@
 ﻿module day17_part02
 
+open AdventOfCode_2017.Modules
 
-
-let path = "day17/day17_input.txt"
+let startInsertions (buffer: int array) (numsteps: int) (stopat: int) =
+    let rec insertElement (index: int) (currentvalue: int) (currentBuffer: int array) =
+        if currentvalue = stopat+1 then
+            currentBuffer[1]
+        else
+            let newindex = (index + numsteps) % currentvalue
+            if newindex = 0 then
+                currentBuffer[1] <- currentvalue
+            insertElement (newindex+1) (currentvalue+1) currentBuffer
+        
+    insertElement 0 1 buffer
 
 let execute() =
-    0
+    let path = "day17/day17_input.txt"
+    let steps = LocalHelper.GetContentFromFile path |> int
+
+    let buffer = [| 0; 0 |]
+    startInsertions buffer steps 50000000
